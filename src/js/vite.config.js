4b7ba19e7ed174049bb0dcf2869e5c214d5d2373
Arianar16 <ariana.rodriguez@frsn.utn.edu.ar>
2026-06-24 20:19:12 -0300

    Configurar multipagina

---
+++ src/js/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    build: {
+    rollupOptions: {
+        input: {
+        main: resolve(__dirname, "index.html"),
+        detalle: resolve(__dirname, "detalle.html"),
+        favoritos: resolve(__dirname, "favoritos.html")
+        }
+    }
+    }
+});
